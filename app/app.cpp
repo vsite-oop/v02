6bce60717f89b66accc6ed5562c0afe5a4cdbcc1
Filip Lončar <filoncar@vsite.hr>
2022-10-17 20:00:06 +0200

    Declaration color class and get, set  red, blue, green implementation

--- app/app.cpp
+++ app/app.cpp
@@ -1,11 +1,40 @@
 #include "app.h"
 #include <algorithm>
 
+//clamp
+//std::clamp(n,0,255)
+
 namespace vsite::oop::v2
 {
 /*
 * class member function implementations
 */
 
+		double color::get_red() const {
+			return red;
+		}
+		double color::get_green() const {
+			return green;
+		}
+		double color::get_blue() const {
+			return blue;
+		}
+
+		void color::set_red(double r) {
+			red = r;
+		}
+		void color::set_green(double g) {
+			green = g;
+		}
+		void color::set_blue(double b) {
+			blue = b;
+		}
+
+		uint32_t color::get_color_ref() const{
+			return 22;
+		}
+		double color::get_luminance() const{
+			return 0.;
+		}
 
 } // namespace
--- app/app.h
+++ app/app.h
@@ -4,11 +4,33 @@
 // from <windows.h>
 #define RGB(r,g,b) ((uint32_t)(((uint8_t)(r)|((uint16_t)((uint8_t)(g))<<8))|(((uint32_t)(uint8_t)(b))<<16)))
 
+//funkcija vraca uint32_t
+
 namespace vsite::oop::v2
 {
 /*
 * class declaration
 */
+
+	class color {
+	private:
+		double red = 0.;
+		double green = 0.;
+		double blue = 0.;
+
+	public:
+		double get_red() const;
+		double get_green() const;
+		double get_blue() const;
+
+	public:
+		void set_red(double r);
+		void set_green(double g);
+		void set_blue(double b);
+
+		uint32_t get_color_ref() const;
+		double get_luminance() const; //luminnace formula rgb
+	};
 
 
 }
