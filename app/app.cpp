3d387b89461b6d0f06133a059a10777c0f21725e
Filip Les <ketvok@gmail.com>
2022-10-25 12:28:52 +0200

    Svi testovi prolaze.

--- app/app.cpp
+++ app/app.cpp
@@ -6,6 +6,36 @@ namespace vsite::oop::v2
 /*
 * class member function implementations
 */
+	double color::get_red() const{  // povratni_tip naziv_klase::naziv_metode()
+		return red;
+	}
 
+	double color::get_green() const{  // Slajd 20 (C++ implementacija)
+		return green;
+	}
+
+	double color::get_blue() const{
+		return blue;
+	}
+
+	void color::set_red(double n) {
+		red = std::clamp(n, 0.0, 1.0);  // Mogu i jednostavno napisati red = n ali onda neæe raditi testovi 3-5. Clamp ugura vrijednost u zadane granice ako je izvan.
+	}
+
+	void color::set_green(double n) {
+		green = std::clamp(n, 0.0, 1.0);
+	}
+
+	void color::set_blue(double n) {
+		blue = std::clamp(n, 0.0, 1.0);
+	}
+
+	uint32_t color::get_color_ref() const {
+		return RGB(red * 255, green * 255, blue * 255);
+	}
+
+	double color::get_luminance() const {
+		return red * 0.2126 + green * 0.7152 + blue * 0.0722;
+	}
 
 } // namespace
--- app/app.h
+++ app/app.h
@@ -9,6 +9,23 @@ namespace vsite::oop::v2
 /*
 * class declaration
 */
+	class color {
+		double red = 0;
+		double green = 0;
+		double blue = 0;
 
+	public:
+		double get_red() const;  // Must be const else returns error! In app.cpp also must be const!
+		double get_green() const;
+		double get_blue() const;
+
+		void set_red(double n);
+		void set_green(double n);
+		void set_blue(double n);
+
+		uint32_t get_color_ref() const;  // Professor says there's a macro for this to work written at the top.
+
+		double get_luminance() const;
+	};
 
 }
