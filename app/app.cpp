81d9ae7ec615f7d981b91353812f3ec93d162cdd
AnMal <junkie_@msn.com>
2023-10-17 08:29:47 +0200

    Antonio Maletić

--- app/app.cpp
+++ app/app.cpp
@@ -1,11 +1,32 @@
 #include "app.h"
 #include <algorithm>
+#include <format>
+#include <ostream>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
 
 namespace vsite::oop::v2
 {
 /*
 * class member function implementations
 */
+    std::string color::to_hex() {
+        std::stringstream ss;
+        ss << "0x" << std::hex;
+        ss << std::setfill('0') << std::setw(2) << xr;
+        ss << std::setfill('0') << std::setw(2) << xg;
+        ss << std::setfill('0') << std::setw(2) << xb;
+        ss << std::endl;
+        return ss.str();
+    }
 
+    uint32_t color::get_color_ref() const {        
+        return RGB(xr, xg, xb);
+    }
+
+    double color::get_luminance() {
+        return red * 0.2126 + green * 0.7152 + blue * 0.0722;
+    }
 
 } // namespace
--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,24 @@
-#include <iostream>
+#include <istream>
 #include <format>
 #include "app.h"
+#include <ostream>
+#include <iostream>
+using vsite::oop::v2::color;
 
 int main()
 {
+    color c;
+    
+    std::cout << "Unesite vrijednosti (0-1): \n";
+
+    std::cout << "Crvena: ";
+    c.set_red(std::cin);
+
+    std::cout << "Zelena: ";
+    c.set_green(std::cin);
+
+    std::cout << "Plava: ";
+    c.set_blue(std::cin);
+
+    std::cout << "Hex vrijednost upisane boje: " << c.to_hex();
 }
