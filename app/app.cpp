6e8e7cf5263d79743ccd9bf275ed78d8c5b583be
Tin Bašić <tin.basic22@gmail.com>
2023-10-23 15:52:47 +0200

    Lab2_TB

--- app/app.cpp
+++ app/app.cpp
@@ -1,5 +1,8 @@
 #include "app.h"
 #include <algorithm>
+#include <format>
+#include <iostream>
+#include <iomanip>
 
 namespace vsite::oop::v2
 {
@@ -7,5 +10,16 @@ namespace vsite::oop::v2
 * class member function implementations
 */
 
+    std::string to_hex(int num) {
+        return std::format("{:06X}", num);
+    }
+
+    uint32_t color::get_color_ref() const {
+        return RGB(red * 255, green * 255, blue * 255);
+    }
+
+    double color::get_luminance() {
+        return red * 0.2126 + green * 0.7152 + blue * 0.0722;
+    }
 
 } // namespace
--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 #include <format>
 #include "app.h"
+#include <ostream>
+#include <iostream>
+using vsite::oop::v2::color;
+using vsite::oop::v2::to_hex;
 
 int main()
 {
+    color c;
+
+    double red;
+    double green;
+    double blue;
+
+    std::cout << "Unesite vrijednosti (0-1): \n";
+
+    std::cout << "Crvena: ";
+    std::cin >> red;
+    c.set_red(red);
+
+    std::cout << "Zelena: ";
+    std::cin >> green;
+    c.set_green(green);
+
+    std::cout << "Plava: ";
+    std::cin >> blue;
+    c.set_blue(blue);
+
+    std::cout << "Hex vrijednost upisane boje: " << "0x" << to_hex(c.get_color_ref());
 }
