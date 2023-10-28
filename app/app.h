68a3cf0715cec3f461b267d29638428a217771b0
Tomislav Pavic <t.pavic@outlook.com>
2023-10-28 15:26:44 +0200

    OBJEKTNO vj 2 solved

--- app/app.h
+++ app/app.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <cstdint>
+#include <algorithm>
+#include <string>
+#include <istream>
+#include <ostream>
 // from <windows.h>
 #define RGB(r,g,b) ((uint32_t)(((uint8_t)(r)|((uint16_t)((uint8_t)(g))<<8))|(((uint32_t)(uint8_t)(b))<<16)))
 
@@ -9,6 +13,37 @@ namespace vsite::oop::v2
 /*
 * class declaration
 */
+    std::string to_hex(int num);
 
+    class color {
+        double red = 0;
+        double green = 0;
+        double blue = 0;
+
+    public:
+        double get_red() const {
+            return red;
+        }
+        double get_green() const {
+            return green;
+        }
+        double get_blue() const {
+            return blue;
+        };
+
+        void set_red(double r) {
+            red = std::clamp(r, 0., 1.);
+        }
+        void set_green(double g) {
+            green = std::clamp(g, 0., 1.);
+        }
+        void set_blue(double b) {
+            blue = std::clamp(b, 0., 1.);
+        }
+
+        uint32_t get_color_ref() const;
+
+        double get_luminance();
+    };
 
 }
--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 #include <format>
 #include "app.h"
+#include <ostream>
+using vsite::oop::v2::color;
+using vsite::oop::v2::to_hex;
 
 int main()
 {
+    color c;
+
+    double red;
+    double green;
+    double blue;
+
+    std::cout << "Unesite vrijednosti: \n";
+
+    std::cout << "Crvena: ";
+    std::cin >> red;
+    c.set_red(red);
+
+    std::cout << "Zelena: ";
+    std::cin >> green;
+    c.set_green(green);
+
+    std::cout << "Plava: ";
+    std::cin >> blue;
+    c.set_blue(blue);
+
+    std::cout << "Hex vrijednost boje" << "0x" << to_hex(c.get_color_ref());
 }
